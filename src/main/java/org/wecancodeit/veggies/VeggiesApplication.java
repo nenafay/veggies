ffc7258550caa3e684aae96837cd3481f1132d12
Nena Park <nena.park@gmail.com>
2018-07-25 14:25:52 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/veggies/VeggiesApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.veggies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeggiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeggiesApplication.class, args);
+	}
+}
