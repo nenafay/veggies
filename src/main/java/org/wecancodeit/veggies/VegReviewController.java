39d9b2ed05e835c1c1fd53245ce2f885c76f35af
Nena Park <nena.park@gmail.com>
2018-07-26 00:22:56 -0400

    all controller tests pass

---
+++ src/main/java/org/wecancodeit/veggies/VegReviewController.java
@@ -0,0 +1,76 @@
+package org.wecancodeit.veggies;
+
+import java.util.Optional;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class VegReviewController {
+	
+	@Resource
+	VegetableRepository veggieRepo;
+	
+	@Resource
+	CategoryRepository categoryRepo;
+	
+	@Resource
+	TagRepository tagRepo;
+	
+	@Resource
+	RecipeRepository recipeRepo;
+	
+	@RequestMapping("/veggie")
+	public String findOneVeggie (@RequestParam (value = "id") 
+			long id, Model model)throws VeggieNotFoundException {
+		Optional<Vegetable>veggie = veggieRepo.findById(id);
+		
+		if(veggie.isPresent()) {
+			model.addAttribute("veggies", veggie.get());
+			return("veggie");
+		}
+		throw new VeggieNotFoundException();
+	}
+	
+	@RequestMapping("/show-veggies")
+	public String findAllVeggies(Model model) {
+		model.addAttribute("veggies", veggieRepo.findAll());
+		return("veggies");
+	}
+	
+	@RequestMapping("/category")
+	public String findOneCategory (@RequestParam (value = "id")
+			long id, Model model)throws CategoryNotFoundException {
+		Optional<Category> category = categoryRepo.findById(id);	
+		
+		if(category.isPresent()) {
+			model.addAttribute("categories", category.get());
+			return("category");
+		}
+		throw new CategoryNotFoundException();
+	}
+	
+	@RequestMapping("/show-categories")
+	public String findAllCategories(Model model) {
+		model.addAttribute("categories", categoryRepo.findAll());
+		return("categories");
+	}
+	
+	@RequestMapping("/tag")
+	public String findOneTag(@RequestParam(value = "id")
+			long id, Model model)throws TagNotFoundException {
+		Optional<Tag>tag = tagRepo.findById(id);
+		
+		if(tag.isPresent()) {
+			model.addAttribute("tags", tag.get());
+			return("tag");
+		}
+		throw new TagNotFoundException() {
+			
+		}
+	}
+}
--- src/test/java/org/wecancodeit/veggies/VegReviewControllerTest.java
+++ src/test/java/org/wecancodeit/veggies/VegReviewControllerTest.java
@@ -1,5 +1,110 @@
 package org.wecancodeit.veggies;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
 public class VegReviewControllerTest {
 
+	@InjectMocks
+	private VegReviewController underTest;
+	
+	@Mock
+	private Vegetable veggie;
+	
+	@Mock
+	private Vegetable anotherVeggie;
+	
+	@Mock
+	private VegetableRepository veggieRepo;
+	
+	@Mock
+	private Category category;
+	
+	@Mock
+	private Category anotherCategory;
+	
+	@Mock
+	private CategoryRepository categoryRepo;
+	
+	@Mock
+	private Tag tag;
+	
+	@Mock
+	private Tag anotherTag;
+	
+	@Mock
+	private TagRepository tagRepo;
+	
+	@Mock
+	private Recipe recipe;
+	
+	@Mock
+	private Recipe anotherRecipe;
+	
+	@Mock
+	private RecipeRepository recipeRepo;
+	
+	@Mock
+	private Model model;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void shouldAddSingleVegReviewToModel() throws VeggieNotFoundException{
+		long arbitraryVeggieId = 1L;
+		when(veggieRepo.findById(arbitraryVeggieId)).thenReturn(Optional.of(veggie));
+		
+		underTest.findOneVeggie(arbitraryVeggieId, model);
+		verify(model).addAttribute("veggies", veggie);
+	
+	}
+	
+	@Test
+	public void shouldAddAllVegReviewsToModel() {
+		Collection<Vegetable> allVeggies = Arrays.asList(veggie, anotherVeggie);
+		when(veggieRepo.findAll()).thenReturn(allVeggies);
+		
+		underTest.findAllVeggies(model);
+		verify(model).addAttribute("veggies", allVeggies);
+	}
+	
+	@Test
+	public void shouldAddSingleCategoryToModel() throws CategoryNotFoundException{
+		long arbitraryCategoryId = 5;
+		when(categoryRepo.findById(arbitraryCategoryId)).thenReturn(Optional.of(category));
+		
+		underTest.findOneCategory(arbitraryCategoryId,model);
+		verify(model).addAttribute("categories", category);
+	}
+	
+	@Test
+	public void shouldAddAllCategoriesToModel(){
+		Collection<Category> allCategories = Arrays.asList(category, anotherCategory);
+		when(categoryRepo.findAll()).thenReturn(allCategories);
+		
+		underTest.findAllCategories(model);
+		verify(model).addAttribute("categories", allCategories);
+	}
+	@Test
+	public void shouldAddSingleTagToModel() throws TagNotFoundException{
+		long arbitraryTagId = 7;
+		when(tagRepo.findById(arbitraryTagId)).thenReturn(Optional.of(tag));
+		
+		underTest.findOneTag(arbitraryTagId,model);
+		verify(model).addAttribute("tags", tag);
+	}
 }
